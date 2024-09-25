5fa67a07da0f8b3b7123aa11e71256e47af0add9
unknown <kdolay@ucsd.edu>
2024-09-24 20:21:51 -0700

    Added basic code to search for artist and tracks, added basic traversal for artist and artists featured.

--- index.js
+++ index.js
@@ -6,12 +6,45 @@ var artistIDsToTraverse = [snoopDoggID]
 var IDtoName = {}
 var IDtoCatalogue = {}
 
+const SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token/'
+const SPOTIFY_API_URL = 'https://api.spotify.com/v1/'
+
 connectAPIs()
+async function searchSpotify(authentication, searchParameters) {
+    const searchParameterObject = new URLSearchParams(searchParameters)
+    const searchAPIURL = SPOTIFY_API_URL + 'search?' + searchParameters
+    console.log(searchAPIURL)
+
+    return fetch(searchAPIURL, {
+        headers: {
+            'Authorization': `Bearer ${authentication["access_token"]}`
+        }
+    }).then(res => res.json())
+    .then(res => {
+        console.log("Spotify Search Results: ")
+        console.log(res)
+        return res
+    })
+}
+
+async function traverseArtists(res) {
+    res.tracks.items.forEach(item => {
+        const trackName = item.name
+        const trackArtists = item.artists
+        if (item.artists.length > 1) {
+            var stringOfArtists = ""
+            for (var artist of trackArtists) {
+                stringOfArtists += `${artist.name} `
+            }
+            console.log(`Name: ${trackName}`)
+            console.log(`Artists: ${stringOfArtists}`)
+        }
+    })
+}
 
 async function authenticateSpotify() {
     var SPOTIFY_ID = process.env.SPOTIFY_CLIENT_ID
     var SPOTIFY_SECRET = process.env.SPOTIFY_CLIENT_SECRET
-
 
     const spotify_parameters = {
         'grant_type': 'client_credentials',
@@ -21,9 +54,8 @@ async function authenticateSpotify() {
 
     const formatted_parameters = new URLSearchParams(spotify_parameters)
 
-    var spotify_url = 'https://accounts.spotify.com/api/token/'
 
-    return fetch(spotify_url, {
+    return fetch(SPOTIFY_TOKEN_URL, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
@@ -36,10 +68,17 @@ async function authenticateSpotify() {
 }
 
 async function connectAPIs() {
-    authenticateSpotify().then(res => {
-        var authenticationDetails = res
-        const access_token = authenticationDetails['access_token']
-        console.log(res)
+
+    //Get Spotify API Authentication Details
+    authenticateSpotify().then(authenticationDetails => {
+        //Search for artist details
+        var trackLimit = 20
+        const SEARCH_DETAILS = `type=track&q=artist:Kendrick Lamar&limit=${trackLimit}`
+        return searchSpotify(authenticationDetails, SEARCH_DETAILS)
+    }).then(artistDetails => {
+        //Traverse the list of artists.
+        //console.log(artistDetails)
+        traverseArtists(artistDetails)
     })
 }
 /*var options = {
