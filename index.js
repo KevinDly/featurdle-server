abf73dddbf33a1b6e153d331305adba9b073faf4
unknown <kdolay@ucsd.edu>
2024-09-25 16:11:56 -0700

    Added artist traversal up to nth degree (specified in .env) into connectAPIs

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 //TODO: Find better way to import env.
 import './env.js'
 
-var artistIDsToTraverse = []
-var visitedArtists = []
+var artistsToExplore = ["Kendrick Lamar"]
+var visitedArtists = new Set([])
 var visitedTracks = []
 var IDtoName = {}
 var IDtoCatalogue = {}
 
 const SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token/'
 const SPOTIFY_API_URL = 'https://api.spotify.com/v1/'
+const ARTIST_DEGREE = process.env.SEARCH_DEGREE
 
 connectAPIs()
 
 async function searchSpotify(authentication, searchParameters) {
     const searchParameterObject = new URLSearchParams(searchParameters)
     const searchAPIURL = SPOTIFY_API_URL + 'search?' + searchParameters
-    console.log(searchAPIURL)
+    //console.log(searchAPIURL)
 
     return fetch(searchAPIURL, {
         headers: {
@@ -23,20 +24,24 @@ async function searchSpotify(authentication, searchParameters) {
         }
     }).then(res => res.json())
     .then(res => {
-        console.log("Spotify Search Results: ")
-        console.log(res)
+        //console.log("Spotify Search Results: ")
+        //console.log(res)
         return res
     })
 }
 
-async function traverseArtists(res) {
+function traverseArtists(res, artistList, artistExplored) {
     res.tracks.items.forEach(item => {
         const trackName = item.name
         const trackArtists = item.artists
         if (item.artists.length > 1) {
             var stringOfArtists = ""
             for (var artist of trackArtists) {
-                stringOfArtists += `${artist.name} `
+                const artistName = artist.name
+                stringOfArtists += `${artistName} `
+                if(!artistList.includes(artistName) && !artistExplored.has(artistName)){
+                    artistList.push(artistName)
+                }
             }
             console.log(`Name: ${trackName}`)
             console.log(`Artists: ${stringOfArtists}`)
@@ -72,23 +77,37 @@ async function authenticateSpotify() {
 async function connectAPIs() {
 
     //Get Spotify API Authentication Details
-    /*authenticateSpotify().then(authenticationDetails => {
-        //Search for artist details
-        var trackLimit = 20
-        const SEARCH_DETAILS = `type=track&q=artist:Kendrick Lamar&limit=${trackLimit}`
-        return searchSpotify(authenticationDetails, SEARCH_DETAILS)
-    }).then(artistDetails => {
-        //Traverse the list of artists.
-        //console.log(artistDetails)
-        traverseArtists(artistDetails)
-    })*/
-
     var authenticationDetails = await authenticateSpotify()
+    var offset = 0
+    var currentDegree = 0
+    var maxToExplore = artistsToExplore.length
+    var currExplored = 0
+    var trackLimit = 5
 
-    var trackLimit = 20
-    const SEARCH_DETAILS = `type=track&q=artist:Kendrick Lamar&limit=${trackLimit}`
-    var artistDetails = await searchSpotify(authenticationDetails, SEARCH_DETAILS)
+    while(artistsToExplore.length > 0 && currentDegree <= ARTIST_DEGREE) {
+        const currentArtistName = artistsToExplore.shift()
 
-    traverseArtists(artistDetails)
+        //Create search query for Spotify
+        const SEARCH_DETAILS = `type=track&q=artist:${currentArtistName}&limit=${trackLimit}&offset=${offset}`
 
+        //Add the current artist to list of names to traverse.
+        var artistDetails = await searchSpotify(authenticationDetails, SEARCH_DETAILS)
+
+        //Check if there are still more songs to traverse for the current artist, if there are dont add to list and put the artist back onto the front of the list.
+        visitedArtists.add(currentArtistName)
+
+        //Look through the featured artists.
+        traverseArtists(artistDetails, artistsToExplore, visitedArtists)
+        currExplored++
+
+        //Update the degrees outward from the original artist we are.
+        if(currExplored >= maxToExplore) {
+            currentDegree++
+            maxToExplore = artistsToExplore.length
+            currExplored = 0
+        }
+    }
+
+    console.log(visitedArtists)
+    console.log(artistsToExplore)
 }
