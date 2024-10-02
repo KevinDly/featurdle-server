e9a42c7052f3220e34572fa81200f69944ed39ee
unknown <kdolay@ucsd.edu>
2024-10-02 15:39:01 -0700

    Moved all Spotify API related functions to spotifyAPI.js, added file handling for local data including reading and saving, added simple checks for when to read and when to write into initializeServer

---
+++ apis/spotifyAPI.js
@@ -0,0 +1,182 @@
+import '../env.js'
+
+const SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token/'
+const SPOTIFY_API_URL = 'https://api.spotify.com/v1/'
+const ARTIST_DEGREE = process.env.SEARCH_DEGREE
+
+//TODO: Move all functions below to a new file for spotify apis.
+export async function searchSpotify(authentication, searchURL) {
+    return fetch(searchURL, {
+        headers: {
+            'Authorization': `Bearer ${authentication["access_token"]}`
+        }
+    }).then(res => {
+        if (res.status !== 200) {
+            throw new Error(`Status code error ${res.status}, ${res.message}`);
+        }
+        return res.json();
+    })
+        .then(res => {
+            return res;
+        });
+}
+//TODO: Implement limit of how many features can be added to the artistList per artist.
+export function traverseArtists(res, artistList, artistExplored, musicToArtist) {
+    //Iterate through all tracks in res.
+    res.tracks.items.forEach(track => {
+        //Iterate through each artist if more than one exists (if the song contains features)
+        const trackName = track.name;
+        const trackArtists = track.artists;
+        const trackID = track.id;
+        const trackAlbum = track.album;
+        const trackImage = trackAlbum.images[0].url;
+
+        //Check if song has no features or related artists, if it doesn't then return.
+        if (track.artists.length <= 1) {
+            return;
+        }
+
+        //Check if the track exists in the list, if not initialize it in the table with an empty object.
+        if (!(trackName in musicToArtist)) {
+            musicToArtist[trackName] = {};
+        }
+
+        //Create key out of artist names.
+        let artistNames = trackArtists.map(artist => artist.name);
+        const artistsNameString = artistNames.sort().toString();
+
+        //Check if same track exists with the same features.
+        if (artistsNameString in musicToArtist[trackName]) {
+            return;
+        }
+
+        let trackList = musicToArtist[trackName];
+
+        //Add artists to list.
+        let trackInformation = {};
+
+        //Add all related artists to the list of artists to explore.
+        for (const artistName of artistNames) {
+            //Only add if we haven't explored the artist before and if the artist isn't set to be explored in the future.
+            if (!artistList.includes(artistName) && !artistExplored.has(artistName)) {
+                artistList.push(artistName);
+            }
+        }
+
+        //Input track information
+        trackInformation['artists'] = artistNames;
+        trackInformation['imageURL'] = trackImage;
+
+        //Add tracks to list if it is unique.
+        trackList[artistsNameString] = trackInformation;
+    });
+}
+export async function authenticateSpotify() {
+    const SPOTIFY_ID = process.env.SPOTIFY_CLIENT_ID;
+    const SPOTIFY_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+
+    const spotify_parameters = {
+        'grant_type': 'client_credentials',
+        'client_id': SPOTIFY_ID,
+        'client_secret': SPOTIFY_SECRET
+    };
+
+    //Format parameters for the fetch.
+    const formatted_parameters = new URLSearchParams(spotify_parameters);
+
+    //Fetch the data.
+    //TODO: Convert to await form?
+    return fetch(SPOTIFY_TOKEN_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: formatted_parameters
+    }).then(res => res.json())
+        .then(res => {
+            return res;
+        });
+}
+export async function populateSpotifyData(spotifyAuthentication, artistsToExplore, visitedArtists, tracksToArtist) {
+    const offset = 0
+    const trackLimit = 50
+
+    let maxToExplore = artistsToExplore.length
+    let currExplored = 0
+    let currentDegree = 0
+
+    const start = Date.now()
+    try {
+        while (artistsToExplore.length > 0 && currentDegree <= ARTIST_DEGREE) {
+            const currentArtistName = artistsToExplore.shift()
+
+            //Create search query for Spotify
+            //let SEARCH_DETAILS = SPOTIFY_API_URL + 'search?' + `type=track&q=artist:${currentArtistName}&limit=${trackLimit}&offset=${offset}`
+            const SEARCH_URL = SPOTIFY_API_URL + 'search?'
+            const SEARCH_PARAMS = new URLSearchParams({
+                type: "track",
+                q: `artist:${currentArtistName}`,
+                limit: `${trackLimit}`,
+                offset: `${offset}`
+            })
+            let SEARCH_DETAILS = SEARCH_URL + SEARCH_PARAMS
+            //Iterate until search pagination is done.
+            while (SEARCH_DETAILS != null) {
+                try {
+                    //Add the current artist to list of names to traverse.
+                    //TODO: Make into while loop, check the "next" key if its null or to continue loop or not.
+                    var artistDetails = await searchSpotify(spotifyAuthentication, SEARCH_DETAILS)
+
+                    //Check if there are still more songs to traverse for the current artist, if there are dont add to list and put the artist back onto the front of the list.
+                    visitedArtists.add(currentArtistName)
+
+                    //Look through the featured artists.
+                    traverseArtists(artistDetails, artistsToExplore, visitedArtists, tracksToArtist)
+                    SEARCH_DETAILS = artistDetails['tracks']['next']
+                }
+                catch (e) {
+                    console.log("Error")
+                    console.log("visitedArtists")
+                    console.log(visitedArtists)
+                    console.log(visitedArtists.size)
+                    console.log("artistsToExplore")
+                    console.log(artistsToExplore)
+                    console.log(artistsToExplore.length)
+                    console.log("Previous Search")
+                    console.log(`${SEARCH_DETAILS}`)
+                    console.log("Tracks to Artist")
+                    console.log(tracksToArtist)
+                    throw e
+                }
+            }
+
+            //console.log(currentDegree)
+            //console.log(artistsToExplore)
+            //console.log(visitedArtists)
+            //TODO: Figure out if we need to add the if statements into the try/catch
+            currExplored++
+
+            //Update the degrees outward from the original artist we are.
+            if (currExplored >= maxToExplore) {
+                console.log(`Updated degree from previous: ${currentDegree}`)
+                currentDegree++
+                maxToExplore = artistsToExplore.length
+                currExplored = 0
+            }
+        }
+    }
+    catch (e) {
+        console.error(e)
+        //console.log(artistsToExplore)
+    }
+
+    const stop = Date.now()
+
+    console.log(`seconds elapsed = ${Math.floor((stop - start) / 1000)}`)
+
+    //TODO: Save files with artist data, including artistsToExplore, visitedArtists, and tracksToArtist
+    console.log(visitedArtists)
+    console.log(artistsToExplore)
+    console.log(tracksToArtist)
+}
+
--- index.js
+++ index.js
@@ -1,205 +1,85 @@
 //TODO: Find better way to import env.
+import { authenticateSpotify, populateSpotifyData } from './apis/spotifyAPI.js'
 import './env.js'
+import fs from 'fs'
+import FileSystemHandle from 'fs/promises'
 
 let artistsToExplore = [process.env.INITIAL_ARTIST]
 let visitedArtists = new Set([])
 let tracksToArtist = {}
-let visitedTracks = []
-let IDtoName = {}
-let IDtoCatalogue = {}
 
-const SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token/'
-const SPOTIFY_API_URL = 'https://api.spotify.com/v1/'
-const ARTIST_DEGREE = process.env.SEARCH_DEGREE
+const DATA_FOLDER = process.env.DATA_FOLDER
+const DATA_FILENAME = process.env.DATA_FILENAME
+const fileLocation = DATA_FOLDER + '/' + DATA_FILENAME
+const SECONDS_TO_DAY = 86400
+const MAX_DAYS_FROM_WRITE = 2
 
 initializeServer()
 
-function initializeServer() {
-    //TODO: Implement file checking for artist and track data, update from spotify only when files older than certain amount of time (specified in env)
-    connectAPIs()
-}
-
-
-//TODO: Move all functions below to a new file for spotify apis.
-async function searchSpotify(authentication, searchURL) {
-    return fetch(searchURL, {
-        headers: {
-            'Authorization': `Bearer ${authentication["access_token"]}`
-        }
-    }).then(res => {
-        if(res.status !== 200) {
-            throw new Error(`Status code error ${res.status}, ${res.message}`)
-        }
-        return res.json()
-    })
-    .then(res => {
-        return res
-    })
-}
-
-
-//TODO: Implement limit of how many features can be added to the artistList per artist.
-function traverseArtists(res, artistList, artistExplored, musicToArtist) {
-    //Iterate through all tracks in res.
-    res.tracks.items.forEach(track => {
-        //Iterate through each artist if more than one exists (if the song contains features)
-        const trackName = track.name
-        const trackArtists = track.artists
-        const trackID = track.id
-        const trackAlbum = track.album
-        const trackImage = trackAlbum.images[0].url
-
-        //Check if song has no features or related artists, if it doesn't then return.
-        if(track.artists.length <= 1) {
-            return
-        }
-
-        //Check if the track exists in the list, if not initialize it in the table with an empty object.
-        if(!(trackName in musicToArtist)) {
-            musicToArtist[trackName] = {}
-        }
-
-        //Create key out of artist names.
-        let artistNames = trackArtists.map(artist => artist.name)
-        const artistsNameString = artistNames.sort().toString()
-
-        //Check if same track exists with the same features.
-        if(artistsNameString in musicToArtist[trackName]) {
-            return
-        }
-
-        let trackList = musicToArtist[trackName]
-
-        //Add artists to list.
-        let trackInformation = {}
-
-        //Add all related artists to the list of artists to explore.
-        for(const artistName of artistNames) {
-            //Only add if we haven't explored the artist before and if the artist isn't set to be explored in the future.
-            if(!artistList.includes(artistName) && !artistExplored.has(artistName)){
-                artistList.push(artistName)
-            }
-        }
-
-        //Input track information
-        trackInformation['artists'] = artistNames
-        trackInformation['imageURL'] = trackImage
-
-        //Add tracks to list if it is unique.
-        trackList[artistsNameString] = trackInformation
-    })
-}
-
-async function authenticateSpotify() {
-    const SPOTIFY_ID = process.env.SPOTIFY_CLIENT_ID
-    const SPOTIFY_SECRET = process.env.SPOTIFY_CLIENT_SECRET
-
-    const spotify_parameters = {
-        'grant_type': 'client_credentials',
-        'client_id': SPOTIFY_ID,
-        'client_secret': SPOTIFY_SECRET
-    }
-
-    //Format parameters for the fetch.
-    const formatted_parameters = new URLSearchParams(spotify_parameters)
-
-    //Fetch the data.
-    //TODO: Convert to await form?
-    return fetch(SPOTIFY_TOKEN_URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: formatted_parameters
-    }).then(res => res.json())
-    .then(res => {
-        return res
-    })
-}
-
-async function connectAPIs() {
-
-    //Get Spotify API Authentication Details
-    const authenticationDetails = await authenticateSpotify()
-
-    const offset = 0
-    const trackLimit = 50
-
-    let maxToExplore = artistsToExplore.length
-    let currExplored = 0
-    let currentDegree = 0
-
-    const start = Date.now()
+async function loadDataFromFile() {
     try {
-    while(artistsToExplore.length > 0 && currentDegree <= ARTIST_DEGREE) {
-        const currentArtistName = artistsToExplore.shift()
+        let fileData = await FileSystemHandle.readFile(fileLocation)
+        let jsonFileData = JSON.parse(fileData)
 
-        //Create search query for Spotify
-        //let SEARCH_DETAILS = SPOTIFY_API_URL + 'search?' + `type=track&q=artist:${currentArtistName}&limit=${trackLimit}&offset=${offset}`
-        const SEARCH_URL = SPOTIFY_API_URL + 'search?'
-        const SEARCH_PARAMS = new URLSearchParams({
-            type: "track",
-            q: `artist:${currentArtistName}`,
-            limit: `${trackLimit}`,
-            offset: `${offset}`
-        })
-        let SEARCH_DETAILS = SEARCH_URL + SEARCH_PARAMS
-        //Iterate until search pagination is done.
-        while(SEARCH_DETAILS != null) {
-            try {
-                //Add the current artist to list of names to traverse.
-                //TODO: Make into while loop, check the "next" key if its null or to continue loop or not.
-                var artistDetails = await searchSpotify(authenticationDetails, SEARCH_DETAILS)
-                
-                //Check if there are still more songs to traverse for the current artist, if there are dont add to list and put the artist back onto the front of the list.
-                visitedArtists.add(currentArtistName)
-
-                //Look through the featured artists.
-                traverseArtists(artistDetails, artistsToExplore, visitedArtists, tracksToArtist)
-                SEARCH_DETAILS = artistDetails['tracks']['next']
-            }
-            catch(e) {
-                console.log("Error")
-                console.log("visitedArtists")
-                console.log(visitedArtists)
-                console.log(visitedArtists.size)
-                console.log("artistsToExplore")
-                console.log(artistsToExplore)
-                console.log(artistsToExplore.length)
-                console.log("Previous Search")
-                console.log(`${SEARCH_DETAILS}`)
-                console.log("Tracks to Artist")
-                console.log(tracksToArtist)
-                throw e
-            }
-        }
-
-        //console.log(currentDegree)
-        //console.log(artistsToExplore)
-        //console.log(visitedArtists)
-        //TODO: Figure out if we need to add the if statements into the try/catch
-        currExplored++
-
-        //Update the degrees outward from the original artist we are.
-        if(currExplored >= maxToExplore) {
-            console.log(`Updated degree from previous: ${currentDegree}`)
-            currentDegree++
-            maxToExplore = artistsToExplore.length
-            currExplored = 0
-        }
-    }
+        artistsToExplore = jsonFileData['artistsToExplore']
+        visitedArtists = new Set(jsonFileData['visitedArtists'])
+        tracksToArtist = jsonFileData['tracksToArtist']
     }
     catch(e) {
-        console.error(e)
-        //console.log(artistsToExplore)
+        console.log(e)
+    }
+}
+
+function saveDataToFile() {
+    const spotifyDataObject = {
+        "visitedArtists": Array.from(visitedArtists),
+        "tracksToArtist": tracksToArtist,
+        "artistsToExplore": artistsToExplore
     }
 
-    const stop = Date.now()
-
-    console.log(`seconds elapsed = ${Math.floor((stop - start) / 1000)}`)
-
-    //TODO: Save files with artist data, including artistsToExplore, visitedArtists, and tracksToArtist
     console.log(visitedArtists)
-    console.log(artistsToExplore)
-    console.log(tracksToArtist)
+    try{
+        fs.writeFile(fileLocation, JSON.stringify(spotifyDataObject), (err) => {
+            if (err) throw err;
+            console.log(`Saved file to ${fileLocation}`)
+        })
+    }
+    catch(e){
+        console.error(e)
+    }
 }
+
+async function initializeServer() {
+    let dataStats = null
+    try { //Check the datafile's metadata, if it exists.
+        dataStats = fs.statSync(fileLocation)
+    }
+    catch(e) {
+        console.log(`${fileLocation} does not exist!`)
+    }
+
+    //If the file existed check the amount of time in days since last modification.
+    let lastModified = dataStats == null ? '' : dataStats.mtime
+    let differenceInDays = -1
+    if(lastModified != '') {
+        let previousDate = new Date(lastModified)
+        let currentDate = new Date()
+        let previousDateMS = previousDate.getTime()
+        let currentDateMS = currentDate.getTime()
+
+        differenceInDays = ((currentDateMS - previousDateMS)/1000)/SECONDS_TO_DAY
+    }
+
+    //If the file didnt exist, or the amount of time is greater than the set time, connect to Spotify to gather data then rewrite file.
+    if(differenceInDays == -1 || differenceInDays > MAX_DAYS_FROM_WRITE) {
+        let spotifyAuthentication = await authenticateSpotify()
+
+        await populateSpotifyData(spotifyAuthentication, artistsToExplore, visitedArtists, tracksToArtist)
+        saveDataToFile()
+    }
+    else { //If the file is still recent enough, load from file.
+        await loadDataFromFile()
+    }
+}
+
+
