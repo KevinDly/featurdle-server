66fa32f0c7fddc09d94eaef4fce63aa068ce4d63
unknown <kdolay@ucsd.edu>
2024-10-03 15:56:18 -0700

    Added basic websocket server code

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ import { authenticateSpotify, populateSpotifyData } from './apis/spotifyAPI.js'
 import './env.js'
 import fs from 'fs'
 import FileSystemHandle from 'fs/promises'
+import { WebSocket, WebSocketServer } from 'ws'
 
 let artistsToExplore = [process.env.INITIAL_ARTIST]
 let visitedArtists = new Set([])
 let tracksToArtist = {}
 
+const SERVER_PORT = Number(process.env.SERVER_PORT)
 const DATA_FOLDER = process.env.DATA_FOLDER
 const DATA_FILENAME = process.env.DATA_FILENAME
 const fileLocation = DATA_FOLDER + '/' + DATA_FILENAME
 const SECONDS_TO_DAY = 86400
+const server = new WebSocketServer({ port: SERVER_PORT })
 
 //TODO: Might need to move these to a config or something instead of env?
 const MAX_DAYS_FROM_WRITE = Number(process.env.MAX_DAYS_FROM_WRITE)
@@ -52,31 +55,29 @@ function saveDataToFile() {
     }
 }
 
-async function initializeServer() {
-
-    //TODO: Move everything here into it's own function specifically for data loading.
+async function initializeData() {
     let dataStats = null
     try { //Check the datafile's metadata, if it exists.
         dataStats = fs.statSync(fileLocation)
     }
-    catch(e) {
+    catch (e) {
         console.log(`${fileLocation} does not exist!`)
     }
 
     //If the file existed check the amount of time in days since last modification.
     let lastModified = dataStats == null ? '' : dataStats.mtime
     let differenceInDays = -1
-    if(lastModified != '') {
+    if (lastModified != '') {
         let previousDate = new Date(lastModified)
         let currentDate = new Date()
         let previousDateMS = previousDate.getTime()
         let currentDateMS = currentDate.getTime()
 
-        differenceInDays = ((currentDateMS - previousDateMS)/1000)/SECONDS_TO_DAY
+        differenceInDays = ((currentDateMS - previousDateMS) / 1000) / SECONDS_TO_DAY
     }
 
     //If the file didnt exist, or the amount of time is greater than the set time, connect to Spotify to gather data then rewrite file.
-    if(ALWAYS_UPDATE_SPOTIFY || differenceInDays == -1 || differenceInDays > MAX_DAYS_FROM_WRITE) {
+    if (ALWAYS_UPDATE_SPOTIFY || differenceInDays == -1 || differenceInDays > MAX_DAYS_FROM_WRITE) {
         console.log("Grabbing authentication from Spotify.")
         let spotifyAuthentication = await authenticateSpotify()
 
@@ -96,5 +97,32 @@ async function initializeServer() {
     console.log(visitedArtists)
     console.log(tracksToArtist)
 }
+
+async function initializeServer() {
+
+    await initializeData()
+
+    server.on('connection', (client) => {
+        console.log("Connection!")
+        console.log(client)
+        client.on('message', (message) => {
+            //TODO: Add code for connecting.
+            // Need code for events:
+            // Entering queue, sending game moves.
+            console.log('Client message!')
+            console.log(message)
+        })
+
+        client.on('close', (message) => {
+            //TODO: Need to properly handle cleanup for related client.
+            // Check what information the message sends.
+            console.log('Client message!')
+            console.log(message)
+        })
+    })
+
+}
+
+
 
 
